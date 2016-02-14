fc0def84f09369247082860156dba328f967ee45
Koen Pauwels <koenpauwels2@gmail.com>
2016-02-14 15:13:20 +0100

    deleting some superfluous files

--- src/Test.hs
+++
@@ -1,9 +0,0 @@
-import Data.Monoid
-foldMap :: Monoid m => (a -> m) -> [a] -> m
-foldMap g = mconcat . map g
-
-foldComposing :: (a -> (b -> b)) -> [a] -> Endo b
-foldComposing f = foldMap (Endo . f)
-
-foldr :: (a -> (b -> b)) -> b -> [a] -> b
-foldr f y0 xs = appEndo (foldComposing f xs) y0
